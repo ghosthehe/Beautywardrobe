bf90f584cabd2832fa78dcfcced05e3088ce0a01
ghosthehe <903470026@qq.com>
2015-11-07 11:08:39 -0800

    美丽衣橱1

---
+++ Beautywardrobe/SearchVC.h
@@ -0,0 +1,13 @@
+//
+//  SearchVC.h
+//  Beautywardrobe
+//
+//  Created by dfxgd on 15/11/7.
+//  Copyright (c) 2015年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchVC : UIViewController
+
+@end
---
+++ Beautywardrobe/SearchVC.m
@@ -0,0 +1,39 @@
+//
+//  SearchVC.m
+//  Beautywardrobe
+//
+//  Created by dfxgd on 15/11/7.
+//  Copyright (c) 2015年 zhiyou. All rights reserved.
+//
+
+#import "SearchVC.h"
+
+@interface SearchVC ()
+
+@end
+
+@implementation SearchVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
